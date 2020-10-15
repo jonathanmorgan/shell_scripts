1d0ce6a65d4c045454cceec0d4ef2f3d1d3dfcee
Jonathan Morgan <jonscomo@gmail.com>
2020-10-15 11:32:37 -0400

    Create parse_file_name_from_path.bash

---
+++ bash/strings/parse_file_name_from_path.bash
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+# ! ----> FUNCTION: parse_file_name_from_path
+#-------------------------------------------------------------------------------
+
+# declare variables - debug
+if [[ -z "$DEBUG" ]]
+then
+    # debug off by default.
+    DEBUG=false
+fi
+
+# declare variables - slash
+if [[ -z "$slash" ]]
+then
+    # default slash to unix-style "/".
+    slash="/"
+fi
+
+# return reference
+file_name_out=-1
+
+function parse_file_name_from_path()
+{
+    # parameters
+    local file_path_in="$1"
+    
+    # declare variables
+    local file_path_token_array=
+    local file_name=
+    
+    # parse the last item off the file path.
+    IFS='_' read -ra file_path_token_array <<< "$file_path_in"
+    for i in "${file_path_token_array[@]}"; do
+        # process "$i"
+        file_name="$i"
+    done
+    
+    file_name_out="$file_name"
+
+    # DEBUG
+    if [[ $DEBUG = true ]]
+    then
+
+        echo "!!!! file name = $file_name_out"
+
+    fi
+
+} #-- END function parse_file_name_from_path() --#
